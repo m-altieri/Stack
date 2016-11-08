8b5f833b79dccbadff2d5e5a523efe0e61a3751b
dfucci <davfuc@gmail.com>
2016-11-08 19:01:38 +0100

    initial commit

---
+++ src/it/uniba/tutorial/Stack.java
@@ -0,0 +1,12 @@
+package it.uniba.tutorial;
+
+public class Stack {
+	
+	public void push(int x) {}
+	public void pop(){}
+	public int peek(){ return -1;}
+	public int size(){ return -1;}
+	public boolean isEmpty() {return false;}
+	
+
+}
---
+++ src/it/uniba/tutorial/tests/StackTest.java
@@ -0,0 +1,24 @@
+package it.uniba.tutorial.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
